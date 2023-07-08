bcd68053d0dcd61862092df34325c4bfec3c0451
Michael Wu <mw10013@gmail.com>
2023-07-07 21:41:32 -0400

    add tailwind

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,15 +2,15 @@ import type { V2_MetaFunction } from "@remix-run/node";
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Remix Sandbox" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+    <div>
+      <h1 className="text-gray-500 text-2xl">Remix Sandbox</h1>
       <ul>
         <li>
           <a
