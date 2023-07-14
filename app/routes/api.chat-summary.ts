8e229a5a27cf47a653c061dfc315c21033963969
Michael Wu <mw10013@gmail.com>
2023-07-14 00:59:59 -0400

    .

--- app/routes/api.chat-summary.ts
+++ app/routes/api.chat-summary.ts
@@ -1,15 +1,11 @@
 import { type ActionArgs } from "@remix-run/node";
 import { StreamingTextResponse, LangChainStream } from "ai";
 import { ChatOpenAI } from "langchain/chat_models/openai";
-import { ConversationChain, LLMChain } from "langchain/chains";
+import { LLMChain } from "langchain/chains";
 import {
-  ChatPromptTemplate,
-  HumanMessagePromptTemplate,
-  SystemMessagePromptTemplate,
-  MessagesPlaceholder,
   PromptTemplate,
 } from "langchain/prompts";
-import { BufferMemory, ConversationSummaryMemory } from "langchain/memory";
+import { ConversationSummaryMemory } from "langchain/memory";
 import type { BaseMessage, StoredMessage } from "langchain/schema";
 import { supabaseAdmin } from "~/lib/supabase";
 import {
@@ -121,40 +117,13 @@ export const action = async ({ request }: ActionArgs) => {
     streaming: true,
   });
   const prompt =
-    PromptTemplate.fromTemplate(`The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+    PromptTemplate.fromTemplate(`The following is a concise conversation between a human and an AI. The AI is laconic and concisely provides specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
 
   Current conversation:
   {chat_history}
   Human: {input}
   AI:`);
-  const chain = new LLMChain({ llm: chat, prompt, memory });
-
-
-  // const chat = new ChatOpenAI({
-  //   modelName: "gpt-3.5-turbo",
-  //   temperature: 0,
-  //   maxRetries: 1,
-  //   streaming: true,
-  // });
-
-  // const prompt = ChatPromptTemplate.fromPromptMessages([
-  //   SystemMessagePromptTemplate.fromTemplate(
-  //     "You are a laconic assistant that responds concisely"
-  //   ),
-  //   // https://js.langchain.com/docs/modules/memory/examples/buffer_memory_chat
-  //   new MessagesPlaceholder("history"),
-  //   HumanMessagePromptTemplate.fromTemplate("{input}"),
-  // ]);
-
-  // const chain = new ConversationChain({
-  //   memory: new BufferMemory({
-  //     returnMessages: true,
-  //     memoryKey: "history",
-  //     chatHistory: await SupaChatMessageHistory.fromId(id),
-  //   }),
-  //   prompt,
-  //   llm: chat,
-  // });
+  const chain = new LLMChain({ llm: chat, prompt, memory, verbose: true });
   chain.call({ input }, [handlers]);
   return new StreamingTextResponse(stream);
 };
