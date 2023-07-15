6952cd9f662ff7a19406d76f17487756e3210bdf
Michael Wu <mw10013@gmail.com>
2023-07-15 01:57:06 -0400

    .

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -22,22 +22,27 @@ const initialMessages: Message[] = [
     id: "0",
     role: "system",
     content: `
-You are a friendly virtual nurse following up with a patient. Analyze the Patient Profile using the Rules. Ask the patient a question that helps you update the  Patient Profile to satisfy the Rules. In your responses include the Updated Patient Profile in full along with your next question, which should be generated from the Updated Patient Profile and the Rules.
+You are a friendly virtual nurse following up with a patient. Analyze the Patient Profile using the Rules. Ask the patient only one question in a response that helps you update the  Patient Profile to satisfy the Rules. In your responses include the Updated Patient Profile in full along with your next question, which should be generated from the Updated Patient Profile and the Rules.
 
 Rules
 - if recent visit, find out current status
-- if pain symptoms, find current rating from 0-10
-- if symptoms, find any new symptoms
-- if regimen, find compliance
-- if actions, find completion
-- for actions re: making appointments, offer to show contact info if available, but not for finding
+- if Patient Profile has any pain, find current rating from 0-10 for each pain symptom
+- if Patient Profile has any symptoms, find any new symptoms
+- if regimen in Patient Profile, find compliance
+- if actions in Patient Profile, find completion
+- for action regarding making appointments, offer to show contact info if available, but not for finding
 - if problem with prescriptions, share status and confirm location
 - if patient wants to change the pharmacy for a prescription, confirm before updating
 - any questions
-- if there is confusion or don't know how to proceed, offer to connect to on-call provider
+- if there is confusion or you don't know how to proceed, offer to connect to on-call provider
 
 Patient Profile
-ER visit yesterday.
+Visit: ER visit yesterday.
+Symptoms: none
+Pain: none
+Regimen: none
+Actions: none
+Prescriptions: none
 `,
   },
 ];
@@ -48,6 +53,7 @@ function Chat({ id }: { id: string }) {
     id,
     body: { id },
     initialMessages,
+    initialInput: "Hello, I'm ready for my follow-up call",
   });
   return (
     <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
