606bf38bc6bb74858bd567898ac147c5b8bbbd0c
Michael Wu <mw10013@gmail.com>
2023-07-26 11:21:09 -0400

    error handling

--- app/routes/api.sn-messages.ts
+++ app/routes/api.sn-messages.ts
@@ -1,4 +1,5 @@
 import { type ActionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { AIMessage, HumanMessage, SystemMessage } from "langchain/schema";
 import type { Message } from "ai";
 import { LangChainStream } from "ai";
@@ -6,18 +7,18 @@ import { ChatOpenAI } from "langchain/chat_models/openai";
 import { StreamingTextResponse } from "~/lib/ai-hacks";
 
 export const action = async ({ request }: ActionArgs) => {
-  const { messages } = await request.json();
-  //   const { content: question } = messages[messages.length - 1];
-  const { stream, handlers } = LangChainStream();
-  const chat = new ChatOpenAI({
-    modelName: "gpt-3.5-turbo",
-    temperature: 0,
-    maxRetries: 1,
-    streaming: true,
-    verbose: true,
-  });
-  chat
-    .call(
+  try {
+    const { messages } = await request.json();
+    //   const { content: question } = messages[messages.length - 1];
+    const { stream, handlers } = LangChainStream();
+    const chat = new ChatOpenAI({
+      modelName: "gpt-3.5-turbo",
+      temperature: 0,
+      maxRetries: 1,
+      streaming: true,
+      verbose: true,
+    });
+    chat.call(
       (messages as Message[]).map((m) =>
         m.role == "user"
           ? new HumanMessage(m.content)
@@ -27,7 +28,10 @@ export const action = async ({ request }: ActionArgs) => {
       ),
       {},
       [handlers]
-    )
-    .catch(console.error);
-  return new StreamingTextResponse(stream);
+    );
+    return new StreamingTextResponse(stream);
+  } catch (e) {
+    console.error(e);
+    return json({ error: e instanceof Error ? e.message : e }, { status: 500 });
+  }
 };
