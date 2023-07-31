16ad7ca952d911ffd9a08efbe5cc2da4abfa518f
Michael Wu <mw10013@gmail.com>
2023-07-31 08:16:18 -0400

    .

--- app/routes/scenarios.$scenario.tsx
+++ app/routes/scenarios.$scenario.tsx
@@ -72,9 +72,11 @@ You are a call center agent. Follow the provided call script in your conversatio
   - No: "I understand. I will call you back at a more convenient time. Goodbye." Goto END
   - Yes: "Great! Let's get started."
 - "How is your knee pain today?"
-  - Bad or painful: "I'm sorry to hear that. It's important that you speak with an onsite provider about your pain. Please hold while I transfer your call to an onsite provider who can assist you further." Goto END
+  - Bad or painful: "I'm sorry to hear that, Karen. Would you like to speak to our onsite provider?"
+    - Yes: "Please hold while I transfer you to Dr. Attila." Goto END
+    - No: "Understood." Goto APPOINTMENT
   - Okay or manageable: "I'm glad to hear that, Karen."
-- "Dr. Patrick recommended you follow up with Dr. Robinson. Which of the following times work for you?" Show Monday at 9am, Tuesday at 10am, Wednesday at 11am as a numbered list.
+- APPOINTMENT: "Dr. Patrick recommended you follow up with Dr. Robinson. Which of the following times work for you?" Show Monday at 9am, Tuesday at 10am, Wednesday at 11am as a numbered list.
   - Chooses time: "Great, Karen. Your appointment with Dr. Robinson is scheduled for {chosen time}. We look forward to seeing you then. Goodbye." Goto END
   - None: "I understand, Karen. Let's try some other options. Which of the following times work for you?" Show Thursday at 1pm and Friday at 2pm as a numbered list." 
     - Chooses time: "Great, Karen. Your appointment with Dr. Robinson is scheduled for {chosen time}. We look forward to seeing you then. Goodbye." Goto END 
@@ -95,10 +97,12 @@ You are a call center agent. Follow the provided call script in your conversatio
   - "Hi, Karen. This is the St. John's Riverside virtual nurse, checking in to see how you're doing today. Are you ready for your follow-up call?"
     - No: "I understand. I will call you back at a more convenient time. Goodbye." Goto END
     - Yes: "Great! Let's get started."
-  - "How is your knee pain today?"
-    - Bad or painful: "I'm sorry to hear that. It's important that you speak with an onsite provider about your pain. Please hold while I transfer your call to an onsite provider who can assist you further." Goto END
+    - "How is your knee pain today?"
+    - Bad or painful: "I'm sorry to hear that, Karen. Would you like to speak to our onsite provider?"
+      - Yes: "Please hold while I transfer you to Dr. Attila." Goto END
+      - No: "Understood." Goto PRESCRIPTION
     - Okay or manageable: "I'm glad to hear that, Karen."
-  - "I am reviewing your chart and see that you were prescribed medication to take at home. Were you able to pick this up from your pharmacy?"
+    - PRESCRIPTION: "I am reviewing your chart and see that you were prescribed medication to take at home. Were you able to pick this up from your pharmacy?"
     - Yes: "Very good."
     - No: "Your prescription was sent to Duane Reade at 100 Broadway. Will you be able to pick it up?"
       - Yes: "Great." Goto QUESTIONS
