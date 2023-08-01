3e94dc51baa608353a35b688000e69e3f2ec2234
Michael Wu <mw10013@gmail.com>
2023-07-31 22:37:13 -0400

    consolidation

--- app/routes/scenarios.$scenario.tsx
+++ app/routes/scenarios.$scenario.tsx
@@ -10,7 +10,7 @@ import type { ButtonProps } from "~/components/ui/button";
 import { Button } from "~/components/ui/button";
 import type { UseChatHelpers } from "ai/react";
 import { useChat } from "ai/react";
-import { cn, nanoid } from "~/lib/utils";
+import { cn } from "~/lib/utils";
 import type { Message } from "ai";
 import {
   Tooltip,
@@ -19,13 +19,8 @@ import {
 } from "~/components/ui/tooltip";
 import { Separator } from "~/components/ui/separator";
 import { useToast } from "~/components/ui/use-toast";
-import {
-  IconArrowDown,
-  IconArrowElbow,
-  IconOpenAI,
-  IconUser,
-} from "~/components/icons";
 import { useInView } from "react-intersection-observer";
+import { customAlphabet } from "nanoid";
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -34,8 +29,17 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
+/**
+ * Return 7 character random string
+ * Taken from nextjs-chat
+ */
+const nanoid = customAlphabet(
+  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+  7
+);
+
+
 export function loader({ params }: LoaderArgs) {
-  console.log({ params });
   const scenario = scenarios.find((s) => s.dynamicSegment === params.scenario);
   if (!scenario) {
     throw new Error(`Scenario not found: ${params.scenario}`);
@@ -75,7 +79,7 @@ You are a call center agent. Follow the provided call script in your conversatio
   - Bad or painful: "I'm sorry to hear that, Karen. Would you like to speak to our onsite provider?"
     - Yes: "Please hold while I transfer you to Dr. Attila." Goto END
     - No: "Understood." Goto APPOINTMENT
-  - Okay or manageable: "I'm glad to hear that, Karen."
+  - Okay or manageable: "I'm glad to hear that, Karen." Goto APPOINTMENT
 - APPOINTMENT: "Dr. Patrick recommended you follow up with Dr. Robinson. Which of the following times work for you?" Show Monday at 9am, Tuesday at 10am, Wednesday at 11am as a numbered list.
   - Chooses time: "Great, Karen. Your appointment with Dr. Robinson is scheduled for {chosen time}. We look forward to seeing you then. Goodbye." Goto END
   - None: "I understand, Karen. Let's try some other options. Which of the following times work for you?" Show Thursday at 1pm and Friday at 2pm as a numbered list." 
@@ -109,30 +113,30 @@ You are a call center agent. Follow the provided call script in your conversatio
     title: "Prescription Reminder",
     dynamicSegment: "prescription",
     content: `
-  # Prompt
+# Prompt
 
-  You are a call center agent. Follow the provided call script in your conversation with a user.
-  
-  # Call Script
-  
-  - "Hi, Karen. This is the St. John's Riverside virtual nurse, checking in to see how you're doing today. Are you ready for your follow-up conversation?"
-    - No: "I understand. I will call you back at a more convenient time. Goodbye." Goto END
-    - Yes: "Great! Let's get started."
-    - "How is your knee pain today?"
-    - Bad or painful: "I'm sorry to hear that, Karen. Would you like to speak to our onsite provider?"
-      - Yes: "Please hold while I transfer you to Dr. Attila." Goto END
-      - No: "Understood." Goto PRESCRIPTION
-    - Okay or manageable: "I'm glad to hear that, Karen."
-    - PRESCRIPTION: "I am reviewing your chart and see that you were prescribed medication to take at home. Were you able to pick this up from your pharmacy?"
-    - Yes: "Very good."
-    - No: "Your prescription was sent to Duane Reade at 100 Broadway. Will you be able to pick it up?"
-      - Yes: "Great." Goto QUESTIONS
-      - No: "Please hold while I connect you to the on-call provider." Goto END
-  - QUESTIONS: "Do you have any questions about your prescription or how to take it?"
-    - Yes: "Please hold while I connect you to the on-call provider." Goto END
-    - No: "Ok."
-  - "Thank you for your time today. If you have any further questions or concerns, please don't hesitate to reach out. Goodbye."
-  - END: ""`,
+You are a call center agent. Follow the provided call script in your conversation with a user.
+
+# Call Script
+
+- "Hi, Karen. This is the St. John's Riverside virtual nurse, checking in to see how you're doing today. Are you ready for your follow-up conversation?"
+  - No: "I understand. I will call you back at a more convenient time. Goodbye." Goto END
+  - Yes: "Great! Let's get started."
+- "How is your knee pain today?"
+  - Bad or painful: "I'm sorry to hear that, Karen. Would you like to speak to our onsite provider?"
+    - Yes: "Please hold while I transfer you to Dr. Attila." Goto END
+    - No: "Understood." Goto PRESCRIPTION
+  - Okay or manageable: "I'm glad to hear that, Karen." Goto PRESCRIPTION
+- PRESCRIPTION: "I am reviewing your chart and see that you were prescribed medication to take at home. Were you able to pick this up from your pharmacy?"
+  - Yes: "Very good." Goto QUESTIONS
+  - No: "Your prescription was sent to Duane Reade at 100 Broadway. Will you be able to pick it up?"
+    - Yes: "Great." Goto QUESTIONS
+    - No: "Please hold while I connect you to the on-call provider." Goto END
+- QUESTIONS: "Do you have any questions about your prescription or how to take it?"
+  - Yes: "Please hold while I connect you to the on-call provider." Goto END
+  - No: "Ok."
+- "Thank you for your time today. If you have any further questions or concerns, please don't hesitate to reach out. Goodbye."
+- END: ""`,
   },
 ];
 
@@ -149,6 +153,64 @@ function composeInitialMessages(systemContent: string): Message[] {
       content: `Hi, Karen. This is the St. John's Riverside virtual nurse, checking in to see how you're doing today. Are you ready for your follow-up conversation?`,
     },
   ];
+}
+
+function IconArrowDown({ className, ...props }: React.ComponentProps<"svg">) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      fill="currentColor"
+      className={cn("h-4 w-4", className)}
+      {...props}
+    >
+      <path d="m205.66 149.66-72 72a8 8 0 0 1-11.32 0l-72-72a8 8 0 0 1 11.32-11.32L120 196.69V40a8 8 0 0 1 16 0v156.69l58.34-58.35a8 8 0 0 1 11.32 11.32Z" />
+    </svg>
+  );
+}
+
+function IconArrowElbow({ className, ...props }: React.ComponentProps<"svg">) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      fill="currentColor"
+      className={cn("h-4 w-4", className)}
+      {...props}
+    >
+      <path d="M200 32v144a8 8 0 0 1-8 8H67.31l34.35 34.34a8 8 0 0 1-11.32 11.32l-48-48a8 8 0 0 1 0-11.32l48-48a8 8 0 0 1 11.32 11.32L67.31 168H184V32a8 8 0 0 1 16 0Z" />
+    </svg>
+  );
+}
+
+function IconOpenAI({ className, ...props }: React.ComponentProps<"svg">) {
+  return (
+    <svg
+      fill="currentColor"
+      viewBox="0 0 24 24"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      className={cn("h-4 w-4", className)}
+      {...props}
+    >
+      <title>OpenAI icon</title>
+      <path d="M22.2819 9.8211a5.9847 5.9847 0 0 0-.5157-4.9108 6.0462 6.0462 0 0 0-6.5098-2.9A6.0651 6.0651 0 0 0 4.9807 4.1818a5.9847 5.9847 0 0 0-3.9977 2.9 6.0462 6.0462 0 0 0 .7427 7.0966 5.98 5.98 0 0 0 .511 4.9107 6.051 6.051 0 0 0 6.5146 2.9001A5.9847 5.9847 0 0 0 13.2599 24a6.0557 6.0557 0 0 0 5.7718-4.2058 5.9894 5.9894 0 0 0 3.9977-2.9001 6.0557 6.0557 0 0 0-.7475-7.0729zm-9.022 12.6081a4.4755 4.4755 0 0 1-2.8764-1.0408l.1419-.0804 4.7783-2.7582a.7948.7948 0 0 0 .3927-.6813v-6.7369l2.02 1.1686a.071.071 0 0 1 .038.052v5.5826a4.504 4.504 0 0 1-4.4945 4.4944zm-9.6607-4.1254a4.4708 4.4708 0 0 1-.5346-3.0137l.142.0852 4.783 2.7582a.7712.7712 0 0 0 .7806 0l5.8428-3.3685v2.3324a.0804.0804 0 0 1-.0332.0615L9.74 19.9502a4.4992 4.4992 0 0 1-6.1408-1.6464zM2.3408 7.8956a4.485 4.485 0 0 1 2.3655-1.9728V11.6a.7664.7664 0 0 0 .3879.6765l5.8144 3.3543-2.0201 1.1685a.0757.0757 0 0 1-.071 0l-4.8303-2.7865A4.504 4.504 0 0 1 2.3408 7.872zm16.5963 3.8558L13.1038 8.364 15.1192 7.2a.0757.0757 0 0 1 .071 0l4.8303 2.7913a4.4944 4.4944 0 0 1-.6765 8.1042v-5.6772a.79.79 0 0 0-.407-.667zm2.0107-3.0231l-.142-.0852-4.7735-2.7818a.7759.7759 0 0 0-.7854 0L9.409 9.2297V6.8974a.0662.0662 0 0 1 .0284-.0615l4.8303-2.7866a4.4992 4.4992 0 0 1 6.6802 4.66zM8.3065 12.863l-2.02-1.1638a.0804.0804 0 0 1-.038-.0567V6.0742a4.4992 4.4992 0 0 1 7.3757-3.4537l-.142.0805L8.704 5.459a.7948.7948 0 0 0-.3927.6813zm1.0976-2.3654l2.602-1.4998 2.6069 1.4998v2.9994l-2.5974 1.4997-2.6067-1.4997Z" />
+    </svg>
+  );
+}
+
+function IconUser({ className, ...props }: React.ComponentProps<"svg">) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      fill="currentColor"
+      className={cn("h-4 w-4", className)}
+      {...props}
+    >
+      <path d="M230.92 212c-15.23-26.33-38.7-45.21-66.09-54.16a72 72 0 1 0-73.66 0c-27.39 8.94-50.86 27.82-66.09 54.16a8 8 0 1 0 13.85 8c18.84-32.56 52.14-52 89.07-52s70.23 19.44 89.07 52a8 8 0 1 0 13.85-8ZM72 96a56 56 0 1 1 56 56 56.06 56.06 0 0 1-56-56Z" />
+    </svg>
+  );
 }
 
 export const MemoizedReactMarkdown: React.FC<Options> = React.memo(
--- app/routes/scenarios._index.tsx
+++ app/routes/scenarios._index.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "@remix-run/node";
+import { scenarios } from "./scenarios.$scenario";
 
 export function loader() {
-  return redirect("/scenarios/appointment");
+  return redirect(`/scenarios/${scenarios[0].dynamicSegment}`);
 }
