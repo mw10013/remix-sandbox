c3550c83204d797db92c878b0512cb33ee188984
Michael Wu <mw10013@gmail.com>
2023-08-04 18:51:52 -0400

    more scripts

---
+++ scripts/agent-react.ts
@@ -0,0 +1,30 @@
+import * as dotenv from "dotenv";
+import * as path from "path";
+import { initializeAgentExecutorWithOptions } from "langchain/agents";
+import { OpenAI } from "langchain/llms/openai";
+import { SerpAPI } from "langchain/tools";
+import { Calculator } from "langchain/tools/calculator";
+
+dotenv.config({ path: path.join(__dirname, "..", ".env") });
+
+(async () => {
+  const model = new OpenAI({ temperature: 0 });
+  const tools = [
+    new SerpAPI(process.env.SERPAPI_API_KEY, {
+      location: "Austin,Texas,United States",
+      hl: "en",
+      gl: "us",
+    }),
+    new Calculator(),
+  ];
+
+  const executor = await initializeAgentExecutorWithOptions(tools, model, {
+    agentType: "zero-shot-react-description",
+    verbose: true,
+  });
+
+  const input = `Who is Olivia Wilde's boyfriend? What is his current age raised to the 0.23 power?`;
+
+  const result = await executor.call({ input });
+  console.log({ result });
+})();
