37bfc27c923d02695d4f82db37df4ebe8de6267a
Michael Wu <mw10013@gmail.com>
2023-08-07 22:19:27 -0400

    add llama

---
+++ scripts/llama-example.ts
@@ -0,0 +1,31 @@
+import * as dotenv from "dotenv";
+import * as path from "path";
+import fs from "fs/promises";
+import { Document, VectorStoreIndex } from "llamaindex";
+
+dotenv.config({ path: path.join(__dirname, "..", ".env") });
+
+async function main() {
+  // Load essay from abramov.txt in Node
+  const essay = await fs.readFile(
+    "./node_modules/llamaindex/examples/abramov.txt",
+    "utf-8"
+  );
+
+  // Create Document object with essay
+  const document = new Document({ text: essay });
+
+  // Split text and create embeddings. Store them in a VectorStoreIndex
+  const index = await VectorStoreIndex.fromDocuments([document]);
+
+  // Query the index
+  const queryEngine = index.asQueryEngine();
+  const response = await queryEngine.query(
+    "What did the author do in college?"
+  );
+
+  // Output response
+  console.log(response.toString());
+}
+
+main();
